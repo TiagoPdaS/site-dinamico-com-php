0943478e436cc9e4a04d47bd6ba75646e8ce48bb
myuser <myemail>
2025-08-20 23:53:44 -0400

    navbar and section banner
    starting the styling of the navbar menu, and the main section

--- index.php
+++ index.php
@@ -4,40 +4,44 @@
     <meta charset="UTF-8">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="A simple PHP site">
     <meta name="keywords" content="PHP, site, dashboard example, palavras chaves do meu site aqui, SEO">
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>site dimanico</title>
 </head>
+
 <body>
     <header>
         <div class="center">
-        <div class="logo">logomarca  </div><!-- Logo da empresa -->
-        <nav class="desktop">
+        <div class="logo left"><img src="images/logo-sem-fundo.png"/> </div><!-- Logo da empresa -->
+        <nav class="desktop right">
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="sobre.php">Sobre</a></li>
                 <li><a href="sobre.php">Serviços</a></li>
                 <li><a href="contato.php">Contato</a></li>
             </ul>
-        <nav class="mobile">
+        </nav>
+        <nav class="mobile right">
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="sobre.php">Sobre</a></li>
                 <li><a href="sobre.php">Serviços</a></li>
                 <li><a href="contato.php">Contato</a></li>
             </ul>
-            </div>
+        </nav>
+            <div class="clear"></div>
+        </div>
         </header>
 
         <section class="banner-principal">
+            <div class="overlay"></div>
             <div class="center">
             <form action="">
-                <h2>Qual o seu melhor e-mail</h2>
+                <h2>Qual o seu melhor e-mail?</h2>
                 <input type="email" name="email" placeholder="Digite seu e-mail" required>
                 <input type="submit" name="acao" value="Cadastrar">
             </form>
